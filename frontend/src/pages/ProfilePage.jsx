49633e3a2725f5d604eac709624fbe7aa1d27668
23120289 <lann99194@gmail.com>
2026-01-12 22:31:44 +0700

    fix bug merge dev

--- frontend/src/pages/ProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProfilePage() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Profile</h1>
-      <p className="text-muted-foreground">Trang quản lý thông tin cá nhân</p>
-    </div>
-  );
-}
